2ac6087583aef8d9cd45d9fb5a17faf0de6fb0e9
natetowsley <natetowsley@gmail.com>
2026-08-21 14:03:58 -0700

    feat: initial index.ts file

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import app from './app.js';
+import logger from './config/logger.js'
+
+const PORT: number = parseInt(process.env.PORT ?? '4000')
+
+
+
+app.listen(PORT, () => {
+    logger.info({ port: PORT }, "app listening")
+});
